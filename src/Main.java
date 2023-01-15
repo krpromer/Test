a185273f1a3dd29e87d87fb728fb4a4dd859f770
krpromer <ammyiik@gmail.com>
2023-01-16 01:13:59 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -15,11 +15,17 @@ public class Main {
             dis = new DataInputStream(fis);
             br = new BufferedReader(new InputStreamReader(dis));
 
+            long total = dis.available()& 0x00000000ffffffffL;
+            long cur = 0;
             String strLine;
             int cnt = 0;
             while((strLine = br.readLine()) != null){
-                if (cnt % 10000 == 0)
-                    L.d(dis.available());
+                if (cnt % 10000 == 0) {
+                    long avail = dis.available() & 0x00000000ffffffffL;
+                    L.d("total = " + total + " avail = " + avail +
+                            " diff = " + (total - dis.available()) + " be = " + avail * 100  + " test = " + (avail * 100 / total));
+                    L.d(100 - (avail * 100 / total) + "%");
+                }
                 cnt++;
             }
             L.d(dis.available());
